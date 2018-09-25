74a1da63161a241a58989a11683c91edd2914cf8
cabanga <manuelcabanga@hotmail.com>
2018-09-25 21:00:26 +0100

    Adiciona o component de listagem das actividades no front end

---
+++ client/src/components/ListsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListsContainer extends Component {
+    render() {
+        return (
+            <div className="">
+                Lists
+            </div>
+        )
+    }
+}
+
+export default ListsContainer;
