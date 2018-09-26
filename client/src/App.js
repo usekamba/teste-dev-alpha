2aa849bb8dbcb0d9e1da65cc3f3476e492ba5641
cabanga <manuelcabanga@hotmail.com>
2018-09-26 21:39:38 +0100

    Ajusta a cor dos cards

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to kamba</h1>
+          <h1 className="App-title">kamba API teste</h1>
         </header>
 
         <ListsContainer />
