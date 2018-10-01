8a8b9c69e180eebcf3986617c284ead8cb1aebb2
cabanga <manuelcabanga@hotmail.com>
2018-10-01 21:15:40 +0100

    Adiciona scope das versoes da API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :activities
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "/api" do
+    scope "/v1" do
+      resources :activities
+    end
+  end
+
 end
