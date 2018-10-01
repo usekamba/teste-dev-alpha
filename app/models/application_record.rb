88230672d7efdbbd660c9e14a2623999fae3ce74
cabanga <manuelcabanga@hotmail.com>
2018-10-01 21:56:47 +0100

    update

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   include HTTParty
 
-  # Retorna dos os itens que estao na API TESTE
+  # Retorna as activities que estao na API TESTE
   def self.all_activities
     HTTParty.get("https://kamba-api-staging.herokuapp.com/v1/users/activities", headers: {"Authorization" => "Token token=\"VS7elyzN7ZqMNcQmQA5Cjgtt\""})
   end
--- client/src/components/Activity.js
+++ client/src/components/Activity.js
@@ -3,12 +3,9 @@ import Currency from 'react-currency-formatter';
 
 
 const Activity = ({activity}) =>
-    
-    
     <div className="col-xs-12 col-md-3" key={activity.id}>
         <div className="card">
             <div className="card-body">
-
                 <h4 className="card-title">{activity.transaction_type}</h4>
                 <p className="text-left card-text">Amount: <Currency quantity={activity.amount ? activity.amount : 0} currency="AOA" pattern="##,### !" decimal="," group="."/></p>
                 <p className="text-left card-text">Subtotal: <Currency quantity={activity.subtotal ? activity.subtotal : 0} currency="AOA" pattern="##,### !" decimal="," group="."/></p>
