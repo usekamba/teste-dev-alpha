640b1f2279c414ce8897946db01ab3abcdcbbb0d
cabanga <manuelcabanga@hotmail.com>
2018-10-01 20:48:36 +0100

    Remove dependencias do frontEnd /Nao usadas

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
