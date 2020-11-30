ee34aa2fc358f0dd0aea3e685be049b87394ac19
GentilPinto <gentil.junior.gp@gmail.com>
2020-11-30 18:33:18 +0100

    [backend] config application.rb to autoload lib path where is included the kamba_service

--- test-rails-backend/config/application.rb
+++ test-rails-backend/config/application.rb
@@ -23,7 +23,7 @@ module TestRailsBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.autoload_paths << "#{Rails.root}/lib"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
