0b6acd35a68aaa7b98e18228e9fa484a0ec17812
GentilPinto <gentil.junior.gp@gmail.com>
2020-11-30 19:18:25 +0100

    [backend] test routing pass

--- test-rails-backend/spec/routing/kambas_routing_spec.rb
+++ test-rails-backend/spec/routing/kambas_routing_spec.rb
@@ -1,28 +1,7 @@
-RSpec.describe KambasController, type: :routing do
+RSpec.describe Api::V1::KambasController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/kambas").to route_to("kambas#index")
-    end
-
-    it "routes to #show" do
-      expect(get: "/kambas/1").to route_to("kambas#show", id: "1")
-    end
-
-
-    it "routes to #create" do
-      expect(post: "/kambas").to route_to("kambas#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/kambas/1").to route_to("kambas#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/kambas/1").to route_to("kambas#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/kambas/1").to route_to("kambas#destroy", id: "1")
+      expect(get: "/api/v1/kambas").to route_to({"format" => :json, "controller" => "api/v1/kambas", "action" => "index"})
     end
   end
 end
