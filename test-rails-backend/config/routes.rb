3c724e99633dfd1a0a77c3fd13d51fb0ffb91050
GentilPinto <gentil.junior.gp@gmail.com>
2020-11-30 18:30:57 +0100

    [backend] setup routes

--- test-rails-backend/config/routes.rb
+++ test-rails-backend/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    namespace :api, defaults: {format: :json} do
+        namespace :v1, defaults: {format: :json} do
+            resources :kambas, only: [:index]
+        end
+    end
 end
